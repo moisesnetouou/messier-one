f7c6121bc182002f848a7960da3ebf7ae8bd182e
moises.neto <moisesnetored@gmail.com>
2022-02-25 00:33:27 -0400

    project: Next.js, Typescript and Chakra UI added, Chakra configured with your custom theme

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import './styles/globals.css'
+import theme from './styles/theme'
+import { ChakraProvider } from '@chakra-ui/react'
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/styles/theme.ts
@@ -0,0 +1,39 @@
+import { extendTheme } from '@chakra-ui/react';
+
+
+const customTheme = extendTheme({
+  fonts: {
+    body: 'Sora, system-ui, sans-serif',
+    heading: 'Sora, Sora, system-ui, sans-serif',
+  },
+  fontWeights: {
+    light: 300,
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  styles: {
+    global: {
+      'html, body': {
+        background: '#050517',
+      },
+    },
+  },
+  radii: {
+    sm: '5px',
+    md: '8px',
+  },
+  fontSizes: {
+    '6xl': '54px',
+  },
+  colors: {
+    blue: {
+      300: '#1475FC',
+      400: '#007AFF',
+      550: '#149AF8',
+      900: '#142847',
+    },
+  },
+});
+
+export default customTheme;
