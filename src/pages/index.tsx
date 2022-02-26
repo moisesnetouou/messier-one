bd8b13583c4079a8eb482cea461ec4bf36ffc2b1
Moisés <moisesnetored@gmail.com>
2022-02-26 11:42:43 -0400

    refac: Interface improvements.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,36 +23,39 @@ export default function Home() {
         direction="column"
         w="100%"
         align="center"
-        h={['100%', '100vh', '100vh']}
+        h={['100%', '100%', '100vh', '100vh']}
         justify="center"
       >
         <Heading
           textAlign="center"
-          fontSize={['2xl', '5xl', '5xl']}
-          my={['2rem', '0', '0']}
+          fontSize={['2xl', '3xl', '5xl', '5xl']}
+          my={['2rem', '2rem', '0', '0']}
         >
-          Bem-vindo ao Messier One
+          Bem-vindo ao{' '}
+          <Text as="span" color="cyan.400">
+            Messier One
+          </Text>
         </Heading>
 
         <Flex
           w="100%"
           justify="space-around"
-          mt={['0', '6rem', '6rem']}
-          direction={['column-reverse', 'row', 'row']}
+          mt={['0', '0', '6rem', '6rem']}
+          direction={['column-reverse', 'column-reverse', 'row', 'row']}
         >
           <Flex
             p="1.5rem"
-            w={['100%', '500px', '500px']}
+            w={['100%', '100%', '500px', '500px']}
             h="350px"
             direction="column"
             justify="space-between"
           >
-            <Text fontSize={['lg', 'xl', 'xl']}>
+            <Text fontSize={['lg', 'lg', 'xl', 'xl']}>
               Problemas na hora de configurar sua máquina pra realizar
               peticionamento? Na Messier One você tem a solução.
             </Text>
 
-            <Text fontSize={['md', 'lg', 'lg']}>
+            <Text fontSize={['md', 'md', 'lg', 'lg']}>
               Realizamos a instalação e configuração completa de maneira remota
               para sua comodidade.
             </Text>
@@ -61,14 +64,15 @@ export default function Home() {
               as="a"
               href="http://api.whatsapp.com/send?1=pt_BR&phone=5592991146464"
               textAlign="center"
-              bg="yellow.200"
-              color="gray.900"
+              bg="cyan.400"
+              color="white"
               w={['100%', '14rem', '14rem']}
-              fontSize="md"
+              fontSize="lg"
               borderRadius="6"
-              p="2"
+              p="3"
               fontWeight="semibold"
               target="_blank"
+              alignSelf={['center', 'center', 'flex-start', 'flex-start']}
             >
               Entre em Contato
             </Text>
@@ -78,8 +82,8 @@ export default function Home() {
             alignSelf="center"
             src="/images/pexel.png"
             alt=""
-            w={['12rem', '20rem', '20rem']}
-            h={['12rem', '20rem', '20rem']}
+            w={['12rem', '12rem', '20rem', '20rem']}
+            h={['12rem', '12rem', '20rem', '20rem']}
             borderRadius="10"
           />
         </Flex>
@@ -88,25 +92,25 @@ export default function Home() {
       <Flex
         direction="column"
         w="100%"
-        my={['8rem', '0', '0']}
-        h={['100%', '100vh', '100vh']}
+        my={['8rem', '8rem', '0rem', '0rem']}
+        h={['100%', '100%', '100%', '100vh', '100vh']}
         justify="center"
       >
         <Flex direction="column" align="center" px="1.5rem">
-          <Heading fontSize={['2xl', '3xl', '3xl']}>
+          <Heading fontSize={['2xl', '2xl', '3xl', '3xl']}>
             Nosso serviço tem como foco praticidade e comodidade
           </Heading>
 
-          <Text mt="1rem" fontSize={['md', 'lg', 'lg']}>
+          <Text mt="1rem" fontSize={['md', 'md', 'lg', 'lg']}>
             Sem precisar sair da sua casa, tudo feito de maneira remota, nosso
             serviço lhe garante que sua máquina vai ter a possibilidade de:
           </Text>
 
           <SimpleGrid
-            columns={[1, 2, 2]}
-            spacing={[5, 20, 20]}
+            columns={[1, 1, 2, 2]}
+            spacing={[5, 10, 20, 20]}
             mt={['2rem', '5rem', '5rem']}
-            fontSize={['md', 'lg', 'lg']}
+            fontSize={['md', 'md', 'lg', 'lg']}
             fontWeight="semibold"
           >
             <Flex
@@ -114,7 +118,7 @@ export default function Home() {
               padding="1.5rem"
               border="1px solid #eaeaea"
               borderRadius="10px"
-              w={['100%', '400px', '400px']}
+              w={['100%', '100%', '300px', '400px']}
               height="120px"
               align="center"
               justify="center"
@@ -130,7 +134,7 @@ export default function Home() {
               padding="1.5rem"
               border="1px solid #eaeaea"
               borderRadius="10px"
-              w={['100%', '400px', '400px']}
+              w={['100%', '100%', '300px', '400px']}
               height="120px"
               align="center"
               justify="center"
@@ -144,7 +148,7 @@ export default function Home() {
               padding="1.5rem"
               border="1px solid #eaeaea"
               borderRadius="10px"
-              w={['100%', '400px', '400px']}
+              w={['100%', '100%', '300px', '400px']}
               height="120px"
               align="center"
               justify="center"
@@ -160,7 +164,7 @@ export default function Home() {
               padding="1.5rem"
               border="1px solid #eaeaea"
               borderRadius="10px"
-              w={['100%', '400px', '400px']}
+              w={['100%', '100%', '300px', '400px']}
               height="120px"
               align="center"
               justify="center"
@@ -170,18 +174,27 @@ export default function Home() {
             </Flex>
           </SimpleGrid>
 
-          <Text
-            mt="10"
-            fontSize="sm"
-            color="red.300"
-            bg="gray.900"
-            p="2"
-            borderRadius="10"
-          >
-            *Aviso: Esses serviços estão apenas disponiveis pra quem utiliza
-            sistema operacional Windows, ainda não oferecemos suporte a Linux ou
-            Mac.
-          </Text>
+          <Flex bg="gray.900" p="5" align="center" my="10" borderRadius="10">
+            <Text
+              fontSize={['sm', 'lg', 'lg']}
+              color="white"
+              p="2"
+              borderRadius="10"
+              fontWeight="semibold"
+            >
+              Serviços disponíveis para:
+            </Text>
+
+            <Flex direction="column" justify="center" align="center" ml="2">
+              <Image
+                src="/images/windows.png"
+                alt=""
+                width="3rem"
+                height="3rem"
+              />
+              <Text color="blue.200">Windows</Text>
+            </Flex>
+          </Flex>
         </Flex>
       </Flex>
 
@@ -197,12 +210,18 @@ export default function Home() {
           <Icon as={BsFillTriangleFill} />
         </Stack>
 
-        <Text
+        <Flex
+          align="center"
           as="a"
           href="http://api.whatsapp.com/send?1=pt_BR&phone=5592991146464"
+          bg="cyan.400"
+          color="white"
+          p="3"
+          borderRadius="10"
+          fontWeight="semibold"
         >
-          <Icon as={BsWhatsapp} fontSize="xl" />
-        </Text>
+          <Text>Entre em Contato</Text>
+        </Flex>
       </Flex>
     </Flex>
   );
